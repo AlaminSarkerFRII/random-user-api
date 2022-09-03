7825fe12a7bac140216ec76f3645b12f83292d7f
alamin <alamin.education1997@gmail.com>
2022-09-03 21:58:32 +0600

    deleted api

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 // users
-const users =[];
+let users =[];
 
 // get all users from /users routes
 router.get("/", (req, res) => {
@@ -13,9 +13,26 @@ router.get("/", (req, res) => {
 // add new user using post api
 router.post("/", (req, res) => {
   const user = req.body;
-  console.log(user,"Users are here")
+//   console.log(user,"Users are here")
   users.push({ ...user, id: uuidv4()});
   res.send(`the user ${user.name} has been added in database`);
 });
+
+// get single user 
+router.get("/:id",(req,res)=>{
+    const {id} = req.params;
+    const singleUser = users.find(user=>user.id === id)
+    res.send(singleUser)
+    console.log("single users")
+})
+
+// delete user 
+router.delete("/:id",(req,res)=>{
+    const {id} = req.params;
+     users = users.filter(user=>user.id !== id)
+    res.send(`user with the id ${id} is deleted `)
+})
+
+
 
 export default router;
