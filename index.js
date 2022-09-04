e2f065d925bdaebad4526c67560dc17adff16275
alamin <alamin.education1997@gmail.com>
2022-09-04 22:32:53 +0600

    finished

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const userRoutes =  require("./routes/users.js")
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 
 // middleware 
